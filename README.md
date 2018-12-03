00086aa1c4bf0c28580eb5c3f2c4535b7308e36f
Upo <hi@upo.one>
2018-12-03 14:51:28 +0000

    end of nov changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://medium.com/google-cloud/google-container-registry-and-portainer-57198bdae070
+https://www.smarthomebeginner.com/traefik-reverse-proxy-tutorial-for-docker
--- traefik/init.sh
+++ traefik/init.sh
@@ -1,10 +1,9 @@
 touch acme.json
 chmod 600 acme.json
+docker network create web
 
 ##Login to GCR
 /snap/bin/gsutil cp gs://upo-scripts/docker/gcr.json .
 cat gcr.json | docker login -u _json_key --password-stdin https://eu.gcr.io
 
-docker-compose up -d
-docker-compose -f nginx.yml up -d
-docker-compose -f portainer.yml up -d
+docker-compose up  -d
